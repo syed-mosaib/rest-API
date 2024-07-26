e641084479e5b8d3d97ac79bf19baf5d07cb2457
syed <syedmosaib231@gmail.com>
2024-07-26 20:38:54 +0530

    commit

--- rest-API.py
+++ rest-API.py
@@ -6,7 +6,7 @@ api = Api(app)
 
 class HelloWorld(Resource):
     def get(self):
-        return{"Hello World"}
+        return{"data" :"Hello World"}
     
 api.add_resource(HelloWorld, "/helloworld")
 
